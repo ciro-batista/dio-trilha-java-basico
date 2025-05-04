06592bf593aa7c2f0d7893847f2d5f24dc079fac
Ciro Batista <cirobatista.dev@gmail.com>
2025-05-03 22:44:44 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,29 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+        // Atribuição
+
+        // =
+        
+            String nome = "Curso"+"de"+"Java";
+
+        // Aritmeticos
+
+        // +
+
+        // -
+
+        // *
+
+        // /
+
+        // %
+
+
+        System.out.println(nome);
+
+
+    }
+    
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        byte idade = 37;
+
+        short ano = 2025;
+
+        // int cep = 02177030; // JAVA CONVERTE PARA OCTAL POR TER O 0 A ESQUERDA, CORRETO USAT STRING.
+
+        String cep = "02177030";
+
+        Long cpf = 11122233304L;
+
+        float pi = 3.14F;
+
+        double salario = 20000.99;
+
+
+        System.out.println(salario);
+
+
     }
 }
+    
